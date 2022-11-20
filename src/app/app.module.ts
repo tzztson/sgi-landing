fa5fd46c76e94506aaa8c5b3bf8939f978faf035
JenisBen <benjamin199551@gmail.com>
2022-11-20 13:02:30 -0800

    add: add the format request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { RequestComponent } from './pages/request/request.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, LandingComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, LandingComponent, RequestComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
