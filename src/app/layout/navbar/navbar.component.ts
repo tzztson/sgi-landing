eeb0313fb7579a2b183bdd34f069706f849ca44f
tzztson <zhenghu61919@gmail.com>
2022-11-22 20:20:48 +0800

    add: added the hamburger menu in Navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  isOpen = false;
+  isSmallOpen = false;
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+    this.isSmallOpen = false;
+  }
+  toggleSmallMenu(): void {
+    this.isSmallOpen = !this.isSmallOpen;
+    this.isOpen = false;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
