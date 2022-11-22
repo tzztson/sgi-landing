685c54a9eb91819afa42bade89d51330740889cb
tzztson <zhenghu61919@gmail.com>
2022-11-22 13:36:28 +0800

    add: add the router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
@@ -8,8 +9,22 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { RequestComponent } from './pages/request/request.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, LandingComponent, RequestComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingComponent,
+    RequestComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'landing', component: LandingComponent },
+      { path: '', component: LandingComponent },
+      { path: 'request', component: RequestComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: 'full' },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
